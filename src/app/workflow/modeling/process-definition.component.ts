55dcc7fac6a617f04d2d8fbc876727413ee8c973
cyanez <cyanez@ontica.org>
2017-05-19 18:33:04 -0500

    Connect process-definition component with our bpmn-js based modeler

--- src/app/workflow/modeling/process-definition.component.ts
+++ src/app/workflow/modeling/process-definition.component.ts
@@ -6,26 +6,71 @@
  *
  */
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { BrowserModule, DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
-import * as modeler from 'bpmn-js/lib/Modeler';
 
 @Component({
   selector: 'process-definition',
-  templateUrl: './process-definition.component.html'
+  templateUrl: './process-definition.component.html',
+  styleUrls: ['process-definition.component.css']
 })
+
 export class ProcessDefinitionComponent {
 
   public url: SafeResourceUrl;
+  public suscriber: any;
 
-  public constructor(private sanitizer: DomSanitizer) {
+  @ViewChild('modeler') public el: ElementRef;
+
+  public constructor(private sanitizer: DomSanitizer, private renderer: Renderer2) {
     this.load();
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('./process-modeler.html');
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('./modeler/process-modeler.html');
+  }
+
+  public createDiagram(): void {
+    this.modeler.createDiagram();
+    this.attachModelerEventHandler();
+  }
+
+  public getDiagram(): void {
+    this.modeler.getXML();
+    this.saveDiagram();
+  }
+
+  public openDiagram(): void {
+    this.modeler.loadXMLFile('vendor/bpmn-js/sample-diagram.bpmn');
+    this.attachModelerEventHandler();
+  }
+
+  public sendInfo(): void {
+    this.modeler.parameters('mensaje');
+  }
+
+  private get modeler() {
+    return this.el.nativeElement.contentWindow.modeler;
+  }
+
+  private attachModelerEventHandler() {
+    this.modeler.suscribeToDoubleClick(this.onModelerDoubleClick);
   }
 
   private load() {
     console.log('loading process definition component ...');
+  }
+
+  private loadXml(xml: any): void {
+    this.modeler.loadXML(xml);
+  }
+
+  private onModelerDoubleClick(element: any): void {
+    alert('elemento recibido en Angular: ' + element);
+  }
+
+  private saveDiagram(): void {
+    this.renderer.listen('window', 'message', (evt) => {
+      let xml = evt.data;
+      console.log(xml);
+    });
   }
 
 }
