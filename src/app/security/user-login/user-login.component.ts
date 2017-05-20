a92cc2b644756b1c9cb892d293f312e528c4d805
cyanez <cyanez@ontica.org>
2017-05-19 20:03:09 -0500

    Integrate user-login component

--- src/app/security/user-login/user-login.component.ts
+++ src/app/security/user-login/user-login.component.ts
@@ -7,9 +7,56 @@
  */
 
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserLoginService } from './user-login.service';
 
 @Component({
   selector: 'user-login',
-  templateUrl: './user-login.component.html'
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css'],
+  providers: [UserLoginService]
 })
-export class UserLoginComponent { }
+
+export class UserLoginComponent {
+
+  public userID = '';
+  public password = '';
+
+  constructor(private router: Router, private loginService: UserLoginService) {
+
+  }
+
+  // region Public methods
+
+  public async authenticate() {
+    if (!this.validateForm()) {
+      return;
+    }
+
+    try {
+      await this.loginService.authenticate(this.userID, this.password);
+      this.router.navigate(['default']);
+
+    } catch (exception) {
+      alert(exception);
+    }
+  }
+
+  // endregion Public methods
+
+  // region Private methods
+
+  private validateForm(): boolean {
+    if (this.userID.length === 0) {
+      alert('Requiero el nombre del usuario.');
+      return false;
+    }
+    if (this.password.length === 0) {
+      alert('Para ingresar al sistema requiero la contraseña.');
+      return false;
+    }
+    return true;
+  }
+
+}
