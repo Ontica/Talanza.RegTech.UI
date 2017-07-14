4df3cc1741330c866baca3d76f42a0b0f7f86a20
cyanez <cyanez@ontica.org>
2017-07-14 14:30:27 -0500

    Add requirement editor initial version

---
+++ src/app/procedures/editor/requirement-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'requirement-grid',
+  templateUrl: './requirement-grid.component.html',
+  styleUrls: ['./requirement-grid.component.scss']
+})
+
+export class RequirementGridComponent {
+  
+}
---
+++ src/app/procedures/editor/tabs/add-requirement-list-tab.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector:'add-requirement-list-tab',
+  templateUrl: './add-requirement-list-tab.component.html',
+  styleUrls: ['./add-requirement-list-tab.component.scss']
+})
+
+export class AddRequirementListTabComponent {
+  @Output() public isCanceled = new EventEmitter<boolean>();
+  
+  public cancel(): void {
+    this.isCanceled.emit(true);
+  }
+
+
+}
