3f08215a96ab68a7f66b8faa926e0da1052b13ae
cyanez <cyanez@ontica.org>
2017-07-14 14:28:36 -0500

    Add status tab initial version

---
+++ src/app/procedures/editor/tabs/status-tab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'status-tab',
+  templateUrl: './status-tab.component.html',
+  styleUrls: ['./status-tab.component.scss']
+})
+
+export class StatusTabComponent {
+
+}
