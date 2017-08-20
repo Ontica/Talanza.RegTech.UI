db89d14a33b456aece650f0a99f5a784d24e3806
cyanez <cyanez@ontica.org>
2017-08-19 19:34:30 -0500

    Add processModel to WorkLlist component

--- src/app/projects/editor/activity-worklist.component.ts
+++ src/app/projects/editor/activity-worklist.component.ts
@@ -5,7 +5,9 @@
  * See LICENSE.txt in the project root for complete license information.
  *
  */
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { ProcessModel } from '../data-types/project';
 
 @Component({
   selector:'activity-worklist',
@@ -18,43 +20,12 @@ import { Component } from '@angular/core';
             <th style="width:35%">Autoridad</th>           
           </tr>
         </thead>
-        <tbody>
-         <!-- <tr *ngFor="let task of taksList; let i = index;">-->
-          <tr>
-            <td style="width:5%">1</td>
-            <td style="width:60%"><a>Desarrollo del plan de trabajo</a></td>
-            <td style="width:35%"></td>            
-          </tr>
-          <tr>
-            <td style="width:5%">2</td>
-            <td style="width:60%"><a>Solicitud de cambio de medidor</a></td>
-            <td style="width:35%">CNH</td>            
-          </tr>
-          <tr>
-            <td style="width:5%">2.1</td>
-            <td style="width:60%"><a>Prperar documento para cambio de medidor</a></td>
-            <td style="width:35%"></td>            
-          </tr>
-           <tr>
-            <td style="width:5%">2.2</td>
-            <td style="width:60%"><a>Enviar solicitud de cambio de medidor</a></td>
-            <td style="width:35%"></td>            
-          </tr>
-           <tr>
-            <td style="width:5%">3</td>
-            <td style="width:60%">Esperar respuesta de la autoridad</td>
-            <td style="width:35%">CNH</td>            
-          </tr>
-           <tr>
-            <td style="width:5%">4</td>
-            <td style="width:60%"><a>Efectuar el cambio de medidores</a><td></td>
-            <td style="width:35%"></td>            
-          </tr>
-           <tr>
-            <td style="width:5%"><5></td>
-            <td style="width:60%"><a>aviso de cambio de medidor</a></td>
-            <td style="width:35%">SENER</td>            
-          </tr>           
+        <tbody>        
+          <tr *ngFor="let step of  processModel.steps">
+            <td style="width:5%">{{step.stepNo}}</td>
+            <td style="width:60%"><a>{{step.name}}</a></td>
+            <td style="width:35%">{{step.involvedParty}}</td>            
+          </tr>          
         </tbody>
       </table>            
             `,
@@ -62,5 +33,7 @@ import { Component } from '@angular/core';
 
 })
 export class ActivityWorklistComponent {
-  
+
+  @Input() public processModel: ProcessModel;
+
 }
