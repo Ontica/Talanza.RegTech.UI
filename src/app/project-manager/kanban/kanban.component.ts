e163bfe291ecd98d37c8273b0986a670c32847a1
cyanez <cyanez@ontica.org>
2017-08-31 17:47:48 -0500

    Delete comments

--- src/app/project-manager/kanban/kanban.component.ts
+++ src/app/project-manager/kanban/kanban.component.ts
@@ -11,32 +11,7 @@ export class KanbanComponent implements OnInit {
 constructor (private elementRef: ElementRef ){}
 
 ngOnInit(){
-  let el = this.elementRef.nativeElement;
-
- /* el.addEventListener('dragenter', (e) => {
-    el.classList.add('over');
-  });
-
-  el.addEventListener('dragover', (e) => {
-    if (e.preventDefault) {
-      e.preventDefault();
-    }
-    el.dataTransfer.setData("text", el.target.id);
-    e.dataTransfer.dropEffect = 'move';
-    return false;
-  });
-
-  el.addEventListener('drop', (e) => {
-    if (e.stopPropagation) {
-      e.stopPropagation(); // Stops some browsers from redirecting.
-    }
-    
-    el.classList.remove('over');
-    let data = JSON.parse(e.dataTransfer.getData('text'));
-    console.log("value " + data);
-    el.target.appendChild(document.getElementById(data));
-    return false;
-  })*/
+ 
 }
 
 public allowDrop(ev: any): void {
