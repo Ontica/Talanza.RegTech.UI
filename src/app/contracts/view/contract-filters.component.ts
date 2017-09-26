01e9aead78969b0635152c36228470646abf6f7b
cyanez <cyanez@ontica.org>
2017-09-25 23:05:59 -0500

    Add search options

--- src/app/contracts/view/contract-filters.component.ts
+++ src/app/contracts/view/contract-filters.component.ts
@@ -25,10 +25,18 @@ import { Contract, EmptyContract, ContractClause } from '../data-types/contract'
                  <option *ngFor="let contract of contractsList" [value]="contract.uid">{{contract.name}}</option>
               </select>
     </td>
+    <td>Sección</td>
+    <td>
+      <select #section class="select-box" style="width:210px;" (change)="onChangeSection(section.value)">
+        <option value="">( Seleccionar un contrato )</option>
+        <option value="Cláusulas">Cláusulas</option>
+        <option value="anexos">Anexos</option>
+      </select>     
+    </td>
     <td>Buscar:</td>
     <td>
-      <input style="width:364px;" type="search " class="text-box ">
-      <button class="btn"><i class="fa fa-search" aria-hidden="true"></i></button>
+      <input style="width:364px;" type="search " class="text-box " [(ngModel)]="keywords">
+      <button class="btn" (click)="onSearch()"><i class="fa fa-search" aria-hidden="true"></i></button>
     </td>
     <td>
       
@@ -43,14 +51,16 @@ export class ContractFiltersComponent implements OnInit {
 
   public contractsList: Contract[] = [];
   public selectedContract: Contract = EmptyContract();
+  public keywords = '';
 
   @Output() clauses = new EventEmitter<ContractClause[]>();
+
+  private section = '';
 
   constructor(private core: CoreService,
     private contractService: ContractService) { }
 
-  ngOnInit() {
-    //console.log(this.clauses);
+  ngOnInit() {   
     this.setInitialValues();
   }
 
@@ -61,11 +71,34 @@ export class ContractFiltersComponent implements OnInit {
       return;
     }
     this.selectedContract = this.contractsList.find((x) => x.uid === uid);
-    this.loadSelectedContractClausesList();
+    
+  }
+
+  public onChangeSection(section: string): void {
+    this.section = section;  
+  }
+
+  public async onSearch() {
+    if (this.selectedContract === EmptyContract()){
+      return;
+    }
+   
+    await this.loadSelectedContractClausesList('');   
+    
+    let clauses: ContractClause[];
+
+    if (this.section === "Cláusulas") {
+      clauses = this.selectedContract.clauses.filter(item => item.section === "Cláusulas");
+    } else {
+      clauses = this.selectedContract.clauses.filter(item => item.section !== "Cláusulas");
+    }
+
+    this.clauses.emit(clauses);       
+    
   }
 
   private setInitialValues(): void {
-    this.loadContractsList();
+    this.loadContractsList();  
   }
 
   private loadContractsList(): void {
@@ -77,19 +110,15 @@ export class ContractFiltersComponent implements OnInit {
       .catch((e) => this.core.http.showAndThrow(e, errMsg));
   }
 
-  private loadSelectedContractClausesList(): void {
+  private async loadSelectedContractClausesList(keywords: string) {
     Assertion.assertValue(this.selectedContract, 'this.selectedContract');
 
     if (this.selectedContract.clauses) {
       return;
     }
-
-    this.contractService.getContractClausesList(this.selectedContract.uid)
+    await this.contractService.searchClauses(this.selectedContract.uid,keywords)    
       .toPromise()
-      .then((x) => {
-        this.selectedContract.clauses = x
-        this.clauses.emit(x);
-      }
+      .then((x) => this.selectedContract.clauses = x       
     );
   }
 
