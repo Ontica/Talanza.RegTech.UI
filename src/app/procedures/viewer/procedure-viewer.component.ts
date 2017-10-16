697c223b38e1af588cbdecb504beb172d82d45cd
cyanez <cyanez@ontica.org>
2017-10-15 19:40:21 -0500

    Display procedure name

--- src/app/procedures/viewer/procedure-viewer.component.ts
+++ src/app/procedures/viewer/procedure-viewer.component.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, HostBinding,
          Input, Output } from '@angular/core';
 
+import { ProcedureService } from '../services/procedure.service';
+
 @Component({
   selector:'procedure-viewer',
   templateUrl: './procedure-viewer.component.html',
-  styleUrls: ['./procedure-viewer.component.scss']
+  styleUrls: ['./procedure-viewer.component.scss'],
+  providers: [ProcedureService]
 })
 
 export class ProcedureViewerComponent {
@@ -15,6 +18,7 @@ export class ProcedureViewerComponent {
   @Input()
   set procedureUID(procedureUID: string) {
     this._procedureUID = procedureUID;
+    this.loadProcedure();
   }
   get procedureUID(): string {
     return this._procedureUID;
@@ -23,6 +27,9 @@ export class ProcedureViewerComponent {
   @Output() public onCloseEvent = new EventEmitter();
 
   public selectedTask: string = 'generalInfo'; 
+  public procedure: any;
+
+  public constructor(private procedureService: ProcedureService) { }
 
   public onClose(): void {
     this.onCloseEvent.emit();
@@ -35,5 +42,24 @@ export class ProcedureViewerComponent {
   public onCloseTaskEditor(): void {
     this.onClose();
   }
+
+  private loadProcedure(): void {
+    const errMsg = 'Ocurrió un problema al intentar guardar.';
+    
+    this.procedureService.getProcedure(this.procedureUID)
+                         .then((procedure) => this.procedure = procedure)
+                         .catch((e) => this.exceptionHandler(e,errMsg));
+ }
+
+ private exceptionHandler(error: any, defaultMsg: string): void {
+  let errMsg = 'Tengo un problema.\n\n';
+
+  if (typeof (error) === typeof (Error)) {
+    errMsg += defaultMsg + '\n\n' + (<Error>error).message;
+  } else {
+    errMsg += defaultMsg + '\n\n' + 'Error desconocido.';
+  }
+  alert(errMsg);
+}
 
 }
