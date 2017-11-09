0f0977ed4cce2fd43b7301d0a242669485d06da1
cyanez <cyanez@ontica.org>
2017-11-09 15:36:45 -0600

    Inbox service

---
+++ src/app/inbox/services/inbox.service.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright (c) 2017 La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ *
+ */
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { CoreService } from '../../core';
+
+
+import { InboxRef } from '../data-types/inbox';
+import { InboxFilter } from '../data-types/inbox-filter';
+
+import { ActivityRef } from '../data-types/activity';
+
+@Injectable()
+export class InboxService {
+
+  public constructor(private core: CoreService) { }
+
+  public getAllInboxItems(): Observable<InboxRef[]> {
+    const path = `v1/inboxes/my-inbox`;
+
+    return this.core.http.get<InboxRef[]>(path);
+  }
+
+  public getInboxItems(filter?: InboxFilter): Observable<InboxRef[]> {    
+    
+        let filterAsString = '';
+    
+        if (filter instanceof InboxFilter) {
+          filterAsString = '?' + filter.toString();   
+        } 
+        const path = `v1/inboxes/my-inbox${filterAsString}`;
+           
+        return this.core.http.get<InboxRef[]>(path)
+  }
+
+}
