368ea4503f934a20cb43e9b949dc1295d2b107f3
cyanez <cyanez@ontica.org>
2018-04-25 09:41:41 -0500

    Rename add-ticket-modal-window.component to add-meeting-modal-window.component

--- src/app/project-meetings/project-meetings.module.ts
+++ src/app/project-meetings/project-meetings.module.ts
@@ -20,7 +20,7 @@ import { ParticipantsComponent } from './parts/participants.component';
 import { SubjectComponent } from './parts/subjects.component';
 import { RecomendationsComponent } from './parts/recomendations.component';
 import { AgreementsComponent } from './parts/agreements.component';
-import { AddTicketModalWindowComponent } from './views/add-ticket-modal.window.component';
+import { AddMeetingModalWindowComponent } from './views/add-meeting-modal.window.component';
 
 import { MeetingsComponent } from './views/project-meetings.component';
 import { MeetingComponent } from  './views/meeting.component';
@@ -32,9 +32,9 @@ import { MeetingComponent } from  './views/meeting.component';
 @NgModule({
   imports: [ProjectMeetingsRoutingModule, CommonModule, FormsModule, ControlsModule],
   declarations: [ProjectMeetingComponent, MeetingRootComponent, ParticipantsComponent,
-                 SubjectComponent, RecomendationsComponent, AddTicketModalWindowComponent, 
+                 SubjectComponent, RecomendationsComponent, AddMeetingModalWindowComponent, 
                  AgreementsComponent, MeetingsComponent, MeetingComponent],
-  exports: [ProjectMeetingComponent, AddTicketModalWindowComponent, MeetingsComponent,
+  exports: [ProjectMeetingComponent, AddMeetingModalWindowComponent, MeetingsComponent,
             MeetingComponent]
 })
 export class ProjectMeetingsModule { }
