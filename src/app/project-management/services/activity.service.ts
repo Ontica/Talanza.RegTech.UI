387742d325e3d42238cc055031825442f5c267df
cyanez <cyanez@ontica.org>
2018-05-03 12:48:54 -0500

    Add error handler in activity.service

--- src/app/project-management/services/activity.service.ts
+++ src/app/project-management/services/activity.service.ts
@@ -17,6 +17,29 @@ import { ActivityFilter } from '../data-types/activity-filter';
 
 import { ActivityRef } from '../data-types/activity';
 
+export enum ActivityServiceErr {
+  POST_ADD_MANUAL_ACTIVITY_ERR =
+        '[POST_ADD_MANUAL_ACTIVITY_ERR] Ocurrió un problema al guardar la actividad manual.', 
+  POST_ADD_MINIMAL_ACTIVITY_ERR =
+        '[POST_ADD_MINIMAL_ACTIVITY_ERR] Ocurrió un problema al guardar la actividad.',
+  GET_ACTIVITY_ERR =
+        '[GET_ACTIVITY_ERR] Ocurrió un problema al leer la actividad.',        
+  GET_SEARCH_ACTIVITIES_ERR =
+        '[GET_SEARCH_ACTIVITIES_ERR] Ocurrió un problema en la busqueda de actividades.', 
+  GET_ACTIVITIES_LIST_AS_GANTT_ERR =
+        '[GET_ACTIVITIES_LIST_AS_GANTT_ERR] Ocurrió un problema al leer la lista de actividades como gantt.',
+  GET_ACTIVITIES_ERR =
+        '[GET_ACTIVITIES_ERR] Ocurrió un problema al leer la lista de actividades.', 
+  PUT_UPDATE_ACTIVITY_ERR = 
+        '[PUT_UPDATE_ACTIVITY_ERR] Ocurrió un problema al actualizar la actividad.', 
+  POST_CLOSE_ACTIVITY_ERR =
+        '[POST_CLOSE_ACTIVITY_ERR] Ocurrió un problema al cerrar la actividad.',              
+  GET_TAGS_ERR =
+        '[GET_TAGS_ERR] Ocurrió un problema al leer la lista de etiquetas.',
+  GET_ACTIVITIES_AS_WORKLIST_ERR =
+        '[GET_ACTIVITIES_AS_WORKLIST_ERR] Ocurrió un problema al leer la lista de actividades como lista de trabajo.',    
+}
+
 @Injectable()
 export class ActivityService {
 
@@ -25,20 +48,26 @@ export class ActivityService {
   public addManualActivity(projectUID: string, activity: Activity): Observable<any[]> {
     const path = `v1/project-management/projects/${projectUID}/activities`;
 
-    return this.core.http.post<any[]>(path, activity);
+    return this.core.http.post<any[]>(path, activity)
+                        .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.POST_ADD_MANUAL_ACTIVITY_ERR, null));
   }
 
   public addActivity(projectUID: string, activity:{name: string, position: number}): Observable<any> {
                               
     const path = `v1/project-management/projects/${projectUID}/activities`;
 
-    return this.core.http.post<any>(path, activity);
+    return this.core.http.post<any>(path, activity)
+                         .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.POST_ADD_MINIMAL_ACTIVITY_ERR, null));
   }
 
   public getActivity(itemId: number): Observable<any> {
     const path = `v1/project-management/activities/${itemId}`;
 
-    return this.core.http.get<any>(path);
+    return this.core.http.get<any>(path)
+                        .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.GET_ACTIVITY_ERR, null));
   }
 
   public searchActivities(projectUID: string, filter: object,
@@ -48,14 +77,18 @@ export class ActivityService {
                                                                           &orderBy=${orderBy}
                                                                           &keywords=${keywords}`;
 
-    return this.core.http.get<Task[]>(path);
+    return this.core.http.get<Task[]>(path)
+                         .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.GET_SEARCH_ACTIVITIES_ERR, null));
   }
 
   public getActivitiesListAsGantt(projectUID: string): Promise<Task[]> {
     const path = `v1/project-management/projects/${projectUID}/as-gantt`;
 
     return this.core.http.get<Task[]>(path)
-      .toPromise();
+                        .catch((e) => 
+                          this.core.http.showAndReturn(e, ActivityServiceErr.GET_ACTIVITIES_LIST_AS_GANTT_ERR, null))
+                        .toPromise();
 
   }
 
@@ -63,7 +96,9 @@ export class ActivityService {
     const path = `v1/project-management/projects/${projectUID}/as-tree`;
 
     return this.core.http.get<ActivityRef[]>(path)
-      .toPromise();
+                          .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.GET_ACTIVITIES_ERR , null))
+                          .toPromise();
 
   }
 
@@ -72,20 +107,26 @@ export class ActivityService {
     const path = `v1/project-management/projects/${projectUID}/activities/${activityUID}`;
 
 
-    return this.core.http.put<ActivityRef>(path, task);
+    return this.core.http.put<ActivityRef>(path, task)
+                         .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.PUT_UPDATE_ACTIVITY_ERR, null));
   }
 
   public closeActivity(projectUID: string,
                       activityUID: string, closeTask: ClosedTask): Observable<ActivityRef> {
     const path = `v1/project-management/projects/${projectUID}/activities/${activityUID}/close`;
 
-    return this.core.http.post<ActivityRef>(path, closeTask);
+    return this.core.http.post<ActivityRef>(path, closeTask)
+                         .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.POST_CLOSE_ACTIVITY_ERR, null));
   }
 
   public getTags(): Observable<any[]> {
     const path = `v1/project-management/tags`;
 
-    return this.core.http.get<any[]>(path);
+    return this.core.http.get<any[]>(path)
+                         .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.GET_TAGS_ERR, null));
   }
 
   public getActivitiesAsWorkList(filter?: ActivityFilter): Observable<ActivityRef[]> {
@@ -99,6 +140,17 @@ export class ActivityService {
     const path = `v1/project-management/projects/as-work-list${filterAsString}`;
 
     return this.core.http.get<any>(path)
+                          .catch((e) => 
+                            this.core.http.showAndReturn(e, ActivityServiceErr.GET_ACTIVITIES_AS_WORKLIST_ERR, null));
   }
+
+  public deleteActivity(projectUID: string, activityUID: string): Observable<any> {
+          
+      const path = `v1/project-management/projects/${projectUID}/activities/${activityUID}`;
+  
+      return this.core.http.delete<any>(path)
+                            .catch((e) => 
+                              this.core.http.showAndReturn(e, ActivityServiceErr.GET_ACTIVITIES_AS_WORKLIST_ERR, null));
+    }
 
 }
