03932fbd11044f2a5a76c1afcede785c3af3fc41
jmcota <jmcota@ontica.org>
2018-05-13 23:46:48 -0400

    Css refactoring colors and buttons (first round)

--- src/app/controls/chips/chips.ts
+++ src/app/controls/chips/chips.ts
@@ -1,18 +1,19 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'chips',
-  templateUrl: './chips.html',
-  styleUrls: ['./chips.scss']
+	selector: 'chips',
+	templateUrl: './chips.html',
+	styleUrls: ['./chips.scss']
 })
 
 export class Chips {
 
-  @Input() items: any;
+	@Input() items: any;
 
-  public deleteItem(item: string): void {   
-    let index =  this.items.findIndex((x) => x.name === item);    
-    this.items[index].selected = false; 
-  }
-  
+	public removeTag(tag: string): void {
+		let index =  this.items.findIndex((x) => x.name === tag);
+
+		this.items[index].selected = false;
+	}
+
 }
