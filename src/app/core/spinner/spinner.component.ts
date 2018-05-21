0f9bd8725c14b3528863b6087b9f20e00acdc470
jmcota <jmcota@ontica.org>
2018-05-20 23:49:09 -0400

    Improve UX and lint code for spinner component

--- src/app/core/spinner/spinner.component.ts
+++ src/app/core/spinner/spinner.component.ts
@@ -1,37 +1,39 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
 import { Component, OnDestroy, OnInit, HostBinding } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 
-import { LoggerService } from '../general/logger.service';
 import { SpinnerState, SpinnerService } from './spinner.service';
 
+
 @Component({
-  selector: 'toh-spinner',
+  selector: 'spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnDestroy, OnInit {
 
-@HostBinding('style.display') public display = 'block';
-@HostBinding('style.position') public position = 'absolute';
+  @HostBinding('style.display') public display = 'block';
+  @HostBinding('style.position') public position = 'absolute';
 
-  public visible = false;
   private spinnerStateChanged: Subscription;
+  visible = false;
 
-  constructor(private loggerService: LoggerService,
-              private spinnerService: SpinnerService) {
+  constructor(private spinnerService: SpinnerService) {
 
   }
 
-  public ngOnInit() {   
-
+  ngOnInit() {
     this.spinnerStateChanged = this.spinnerService.spinnerState
-      .subscribe((state: SpinnerState) => {
-        this.visible = state.show;
-      
-      });
+                                   .subscribe((state: SpinnerState) => this.visible = state.show );
   }
 
-  public ngOnDestroy() {
+  ngOnDestroy() {
     this.spinnerStateChanged.unsubscribe();
   }
 
--- src/app/core/spinner/spinner.service.ts
+++ src/app/core/spinner/spinner.service.ts
@@ -1,10 +1,19 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 
+
 export interface SpinnerState {
   show: boolean;
 }
+
 
 @Injectable()
 export class SpinnerService {
@@ -15,15 +24,15 @@ export class SpinnerService {
     // no-op
   }
 
-  public get spinnerState(): Observable<SpinnerState> {
+  get spinnerState(): Observable<SpinnerState> {
     return this._spinnerSubject.asObservable();
   }
 
-  public show() {    
+  show() {
     this._spinnerSubject.next(<SpinnerState> { show: true });
   }
 
-  public hide() {
+  hide() {
     this._spinnerSubject.next(<SpinnerState> { show: false });
   }
 
