9207d68f0950c1c47b85f33f9a537d9de73a7955
jmcota <jmcota@ontica.org>
2018-06-12 21:46:42 -0400

    Move tree activity-inline-editor to its own component

---
+++ src/app/project-management/activity-tree/activity-inline-editor.component.ts
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
+import { Component, EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'activity-inline-editor',
+  template: `
+    <input #txtActivityName type="text"
+      class='text-box inline-editor-text-box'
+      (keyup.esc)='onCancel()'
+      (keyup.enter)='onInsert(txtActivityName.value)'
+      placeholder= 'Agregar una actividad'>
+    <button class='btn' (click)='onCancel()'>Cancelar</button>
+  `,
+  styleUrls: ['./activity-inline-editor.component.scss']
+})
+export class ActivityInlineEditorComponent {
+
+  @Output() cancel = new EventEmitter<void>();
+
+  @Output() insert = new EventEmitter<string>();
+
+  onCancel() {
+    this.cancel.emit();
+  }
+
+  onInsert(activityName: string) {
+    this.insert.emit(activityName);
+  }
+
+}
