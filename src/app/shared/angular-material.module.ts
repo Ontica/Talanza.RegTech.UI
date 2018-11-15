87e2f5e56c9e22a543db62070f83910a70d91b1b
jmcota <jmcota@ontica.org>
2018-11-14 20:18:02 -0500

    Add Moment.js to use Material MomentDateAdapter instead of Native

--- src/app/shared/angular-material.module.ts
+++ src/app/shared/angular-material.module.ts
@@ -13,11 +13,59 @@ import {
   MatDialogModule,
   MatFormFieldModule,
   MatMenuModule,
-  MatNativeDateModule,
   MatProgressSpinnerModule,
   MatTabsModule
 } from '@angular/material';
 
+
+import {
+  MomentDateAdapter,
+  MatMomentDateModule
+} from '@angular/material-moment-adapter';
+
+
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from '@angular/material/core';
+
+
+import * as moment from 'moment';
+
+
+const APP_DEFAULT_DATE_LOCAL = 'es-MX';
+
+moment.updateLocale(APP_DEFAULT_DATE_LOCAL, {
+  monthsShort: 'Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sep_Oct_Nov_Dic'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MMM/YYYY',
+    LL: 'DD [de] MMMM [de] YYYY',
+    LLL: 'DD [de] MMMM [de] YYYY LT',
+    LLLL: 'dddd DD [de] MMMM [de] YYYY LT'
+  },
+  week: {
+    dow: 7,
+    doy: 1
+  }
+});
+
+moment.locale(APP_DEFAULT_DATE_LOCAL);
+
+
+export const DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MMM/YYYY',
+    monthYearLabel: 'MMM-YYYY',
+    dateA11yLabel: 'DD/MMM/YYYY',
+    monthYearA11yLabel: 'MMM-YYYY',
+  },
+};
 
 @NgModule({
 
@@ -27,7 +75,7 @@ import {
     MatDialogModule,
     MatFormFieldModule,
     MatMenuModule,
-    MatNativeDateModule,
+    MatMomentDateModule,
     MatProgressSpinnerModule,
     MatTabsModule
   ],
@@ -38,8 +86,16 @@ import {
     MatDialogModule,
     MatFormFieldModule,
     MatMenuModule,
+    MatMomentDateModule,
     MatProgressSpinnerModule,
     MatTabsModule
   ],
+
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: APP_DEFAULT_DATE_LOCAL },
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS }
+  ]
+
 })
 export class AngularMaterialModule { }
