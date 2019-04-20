40e13a15c5557b4d75a6ea30a9ba917c10549df9
jmcota <jmcota@ontica.org>
2019-04-19 23:14:42 -0400

    Use new widget components in side navigation bar

--- src/app/shared/main-layout/sidebar/sidebar.component.ts
+++ src/app/shared/main-layout/sidebar/sidebar.component.ts
@@ -1,14 +1,15 @@
 /**
  * @license
- * Copyright (c) 2017 La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
  *
  * See LICENSE.txt in the project root for complete license information.
- *
  */
 
 import { Component, Input } from '@angular/core';
 
 import { ProjectStore } from '@app/store/project.store';
+import { Project } from '@app/models/project-management';
+import { Contact } from '@app/models/regulation';
 
 
 @Component({
@@ -20,6 +21,29 @@ export class SidebarComponent {
 
   @Input() layoutType: string;
 
+
   constructor(public projectStore: ProjectStore) {}
+
+
+  onSelectProject(project: Project) {
+    if (project && project.uid) {
+      this.projectStore.selectProject(project);
+    }
+  }
+
+
+  onSelectedProjects(projectList: Project[]) {
+    console.log('onSelectedProjects', projectList);
+  }
+
+
+  onSelectedResponsibles(responsiblesList: Contact[]) {
+    console.log('onSelectedResponsibles', responsiblesList);
+  }
+
+
+  onSelectedThemes(themesList: string[]) {
+    console.log('onSelectedThemes', themesList);
+  }
 
 }
